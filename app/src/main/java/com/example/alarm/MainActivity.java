64fa058ca1b1584687d65bec71d785b26ad7798b
nicol <nicoletaleonte24@gmail.com>
2019-05-08 17:37:58 +0300

    Alarm without notification

--- app/src/main/java/com/example/alarm/MainActivity.java
+++ app/src/main/java/com/example/alarm/MainActivity.java
@@ -2,16 +2,68 @@ package com.example.alarm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
     TimePicker timePicker;
+    private long alarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        timePicker = (TimePicker) findViewById(R.id.timePicker);
+
+        findViewById(R.id.buttonSetAlarm).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar calendar = Calendar.getInstance();
+
+                if (Build.VERSION.SDK_INT >= 23) {
+                    calendar.set(
+                            calendar.get(Calendar.YEAR),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH),
+                            timePicker.getHour(),
+                            timePicker.getMinute(),
+                            0
+                    );
+                } else {
+                    calendar.set(
+                            calendar.get(Calendar.YEAR),
+                            calendar.get(Calendar.MONTH),
+                            calendar.get(Calendar.DAY_OF_MONTH),
+                            timePicker.getCurrentHour(),
+                            timePicker.getCurrentMinute(),
+                            0
+                    );
+                }
+                setAlarm(calendar.getTimeInMillis());
+
+            }
+        });
+    }
+
+    public void setAlarm(long timeInMilliis) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, MyAlarm.class);
+
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0,intent,0);
+        alarmManager.setRepeating(AlarmManager.RTC, timeInMilliis,
+                AlarmManager.INTERVAL_DAY, pendingIntent);
+
+        Toast.makeText(this, "Alarm is set." , Toast.LENGTH_SHORT).show();
     }
 }
